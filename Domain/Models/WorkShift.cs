76cb0bd2d174fc02b6efd531ede1a5aabf0aac63
Mads Hylleberg Nielsen <madshyllebergn@gmail.com>
2022-10-17 11:06:53 +0200

    // ¤ Version 0.2 - Basic Setup // * Added Models

---
+++ Domain/Models/WorkShift.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Models;
+
+public class WorkShift
+{ 
+    // # Fields
+    public int ShiftId {get; set;}
+    public string Date {get; set; } // ? Which date?
+    public string Time {get; set; } // ? When?
+    public Worker Worker {get; set; } // ? Who works the shift?
+    public string BreakAmount{get; set; } // ? How much break the worker has ex. 15 min
+    
+    
+    // ¤ Constructor
+    public WorkShift(string date, string time, Worker worker, string breakAmount)
+    {
+        Date = date;
+        Time = time;
+        Worker = worker;
+        BreakAmount = breakAmount;
+    }
+}
---
+++ Domain/Models/Worker.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models;
+
+public class Worker
+{
+    // # Fields
+    public int WorkerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Mail{ get; set; } 
+    public string Address { get; set; }
+    
+    // TODO: Authentication
+}
