4e7c4e53f19320a74e5071b4af4ba50cfdc811e6
Bjarke <bjarkebp@live.dk>
2022-12-01 11:41:40 +0100

    Added it to ISender.cs

--- RabbitMQ/Interfaces/ISender.cs
+++ RabbitMQ/Interfaces/ISender.cs
@@ -6,10 +6,16 @@ namespace RabbitMQ.Interfaces;
 public interface ISender
 {
     //Worker methods
+    
     void CreateWorker(Worker toSend);
-
+    void EditWorker(Worker toSend);
+    void RemoveWorker(int workerId);
+    void GetWorkerById(int workerId);
+    
     //Shift methods
     void CreateShift(WorkShift toSend);
     void EditShift(WorkShift toSend);
     void RemoveShift(int shiftId);
+    void GetShiftById(int shiftId);
+    
 }
